b5f59204475169c1502627b2d720c6b14f4b1879
Tristan <tristan.ggouveia.fr@gmail.com>
2024-06-10 17:23:45 +0200

    RecuperationIdEtMdp

--- frontend/src/Component/Interface.tsx
+++ frontend/src/Component/Interface.tsx
@@ -2,12 +2,16 @@ import React, { useState } from 'react'
 import './Interface.css'
 import Login from './Interface/Login'
 import Accueil from './Interface/Accueil'
+import MonCompte from './Interface/MonCompte'
+import RecuperationIdEtMdp from './Interface/RecuperationIdEtMdp'
 
 function Interface(props: { connecter: any, setConnecter: any, interfaceNumber: any, setInterfaceNumber: any }) {
     return (
         <div className="Interface">
             {props.interfaceNumber === 0 ? <Login setInterfaceNumber={props.setInterfaceNumber} setConnecter={props.setConnecter} /> : null}
             {props.interfaceNumber === 1 ? <Accueil /> : null}
+            {props.interfaceNumber === 2 ? <MonCompte /> : null}
+            {props.interfaceNumber === 3 ? <RecuperationIdEtMdp setInterfaceNumber={props.setInterfaceNumber} /> : null}
 
         </div>
     )
