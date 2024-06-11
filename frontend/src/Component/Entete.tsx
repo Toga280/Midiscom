42e336cdaf9a56fdd3a1341e5b27f3cc712bdccc
Tristan <tristan.ggouveia.fr@gmail.com>
2024-06-11 12:34:22 +0200

    css login + prettierrc

--- frontend/src/Component/Entete.tsx
+++ frontend/src/Component/Entete.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
 import './Entete.css'
 // @ts-ignore
--- frontend/src/Component/Interface/Login.tsx
+++ frontend/src/Component/Interface/Login.tsx
@@ -1,38 +1,81 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './Login.css'
 
-function Login(props: { setInterfaceNumber: any, setConnecter: any }) {
-    const [id, setId] = useState(String)
-    const [mdp, setMdp] = useState(String)
+function Login(props: { setInterfaceNumber: any; setConnecter: any }) {
+  const [id, setId] = useState('')
+  const [mdp, setMdp] = useState('')
 
-    const handleChangeId = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setId(event.target.value)
-    }
-    const handleChangeMdp = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setMdp(event.target.value)
-    }
+  const handleChangeId = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setId(event.target.value)
+  }
 
-    const connection = () => {
-        props.setConnecter(true)
-        props.setInterfaceNumber(1)
-    }
+  const handleChangeMdp = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMdp(event.target.value)
+  }
 
-    return (
-        <div>
-            <div>
-                <p>Identification</p>
-                <p>Espace Abonné</p>
-            </div>
-            <div>
-                <textarea value={id} onChange={handleChangeId} />
-            </div>
-            <div>
-                <textarea value={mdp} onChange={handleChangeMdp} />
-            </div>
-            <div onClick={connection}>SE CONNECTER</div>
-            <div onClick={() => props.setInterfaceNumber(3)}>Mot de passe ou Identifiant oublié ?</div>
+  const connection = () => {
+    props.setConnecter(true)
+    props.setInterfaceNumber(1)
+  }
+
+  const textArea2Ref = useRef<HTMLTextAreaElement | null>(null)
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      if (textArea2Ref.current) {
+        textArea2Ref.current.focus()
+      }
+    }
+  }
+
+  const handleKeyDownValide = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      connection()
+    }
+  }
+
+  return (
+    <div className='Login'>
+      <div className='Login-text'>
+        <p>Identification</p>
+        <p className='Login-text-espace-abonne'>Espace Abonné</p>
+      </div>
+      <div className='Login-container-textarea'>
+        <div className='Login-textarea'>
+          <p>Identifiant</p>
+          <textarea
+            value={id}
+            onChange={handleChangeId}
+            rows={1}
+            onKeyDown={handleKeyDown}
+          />
         </div>
-    )
+        <div className='Login-textarea'>
+          <p>Mot de passe</p>
+          <textarea
+            ref={textArea2Ref}
+            value={mdp}
+            onChange={handleChangeMdp}
+            rows={1}
+            onKeyDown={handleKeyDownValide}
+          />
+        </div>
+      </div>
+      <div className='Login-button'>
+        <div className='Login-button-se-connecter' onClick={connection}>
+          SE CONNECTER
+        </div>
+        <div
+          className='Login-button-mdp-oublié'
+          onClick={() => props.setInterfaceNumber(3)}
+        >
+          Mot de passe ou Identifiant oublié ?
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Login
