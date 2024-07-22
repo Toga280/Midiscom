85b109900c2e226b729b2ea8bfbbfdac7624ce15
Tristan <tristan.ggouveia.fr@gmail.com>
2024-07-22 11:01:40 +0200

    PORT backend pour mise en service

--- backend/src/settings/settings.ts
+++ backend/src/settings/settings.ts
@@ -1,12 +1,14 @@
 const express = require('express')
 const cors = require('cors')
+//const PORT = 5000
+const PORT = process.env.PORT
 
 export const app = express()
 
 export async function SetUpPortAndCore(): Promise<void> {
   app.use(cors())
   app.use(express.json())
-  app.listen(/*process.env.PORT*/ 5000, () => {
+  app.listen(PORT, () => {
     console.log('Connexion au port', /*process.env.PORT*/ 5000, 'réussie')
   })
 }
